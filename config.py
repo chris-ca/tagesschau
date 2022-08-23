eaada2ddbf6e47a90c795efb45b47720587fff6b
Chris Weyland <github@nextmail.cc>
2022-08-23 20:01:38 +0200

    initial commit

---
+++ config.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+import os
+
+cfg = {
+    'cache_duration' : 60*30,
+    'cache_path' : os.path.abspath(os.path.dirname(__file__))+'/.cache'
+}
---
+++ tagesschau.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+from config import cfg
+import requests
+from diskcache import Cache
+import jinja2
+import simplemailer
+cache = Cache(cfg['cache_path'])
+
+@cache.memoize(expire=cfg['cache_duration'])
+def hp_news():
+    r = requests.get('https://www.tagesschau.de/api2/homepage/')
+    contents = r.json()
+    return contents['news']
+news_entries = hp_news()
+
+last_entry = news_entries[0]
+
+templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateEnv = jinja2.Environment(loader=templateLoader)
+TEMPLATE_FILE = "news.html"
+template = templateEnv.get_template(TEMPLATE_FILE)
+html = template.render(entries=news_entries)
+
+simplemailer.SimpleSMTP.from_config() \
+.subject('News update: {title} '.format(**last_entry)) \
+.html(html) \
+.send()
