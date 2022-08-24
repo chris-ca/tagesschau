ea0d803a086a5ed08a37c59d5f737947e90d28d8
Chris Weyland <github@nextmail.cc>
2022-08-24 07:15:51 +0200

    fix template path

--- tagesschau.py
+++ tagesschau.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from config import cfg
+import os
 import requests
 from diskcache import Cache
 import jinja2
@@ -15,12 +16,11 @@ news_entries = hp_news()
 
 last_entry = news_entries[0]
 
-templateLoader = jinja2.FileSystemLoader(searchpath="./")
+templateLoader = jinja2.FileSystemLoader(searchpath=str(os.path.dirname(os.path.abspath(__file__))))
 templateEnv = jinja2.Environment(loader=templateLoader)
 TEMPLATE_FILE = "news.html"
 template = templateEnv.get_template(TEMPLATE_FILE)
 html = template.render(entries=news_entries)
-
 simplemailer.SimpleSMTP.from_config() \
 .subject('News update: {title} '.format(**last_entry)) \
 .html(html) \
